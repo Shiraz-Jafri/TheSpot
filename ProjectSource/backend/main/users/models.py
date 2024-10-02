768ab6bc02f89001b323429739718f71ac4a5e08
Shiraz Jafri <shijafri@iu.edu>
2024-10-02 10:30:34 -0400

    Worked with models

--- ProjectSource/backend/main/users/models.py
+++ ProjectSource/backend/main/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+
+    first_name = models.CharField(max_length=50, default='')
+    last_name = models.CharField(max_length=50, default='')
+    email = models.EmailField(max_length=100, default='example@gmail.com')
+    username = models.CharField(max_length=100, default='')
+    password = models.CharField(max_length=50, default='')
+
+    def __str__(self):
+        return self.first_name
+    
