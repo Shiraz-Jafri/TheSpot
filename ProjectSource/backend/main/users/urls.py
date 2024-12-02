53337da9f4b31282358ea0a84447121958b3fae2
Shiraz-Jafri <150310230+Shiraz-Jafri@users.noreply.github.com>
2024-12-01 20:31:10 -0500

    Delete ProjectSource/backend/main directory

--- ProjectSource/backend/main/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views as user_views
-
-urlpatterns = [
-    path('login/', user_views.login),
-    path('logout/', user_views.logout),
-    path('register', user_views.register),
-]
